964bb297f246612675be3eb4c666f3f6cf689f19
Meghal <meghal.nit@gmail.com>
2017-01-23 10:09:56 +0530

    Worked on quiz models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class quiz_dataAdmin(admin.ModelAdmin):
+	list_display=["id","quiz_name","quiz_description","quiz_image","quiz_type","modified","created"]
+admin.site.register(quiz_data,quiz_dataAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class quiz_data(models.Model):
+	quiz_name=models.CharField(max_length=24,null=False,blank=False)
+	quiz_description=models.CharField(max_length=512,null=False,blank=True)
+	quiz_image=models.CharField(max_length=512,null=False,blank=True)
+	quiz_type=models.IntegerField(default=0)
+	modified= models.DateTimeField(auto_now=True,auto_now_add=False)
+	created= models.DateTimeField(auto_now=False,auto_now_add=True)
